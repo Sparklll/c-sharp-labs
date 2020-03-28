ce87d11ee77aa2db1e63ef979ac44d1cc16c2aaf
Eugene <evgromadko@gmail.com>
2020-03-28 10:59:20 +0300

    implement lab3

---
+++ lab3/Room/Room/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room room = new Room();
+            Room hangar = new Room(
+                "Hangar", 
+                "Premises for livestock and grain storage", 
+                8.5, 
+                30, 
+                10, 
+                true, 
+                true);
+            
+            Console.WriteLine(room);
+            Console.WriteLine(hangar);
+        }
+    }
+}
